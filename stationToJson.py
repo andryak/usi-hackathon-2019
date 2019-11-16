a3983c01fd9817d2446adb78edb72599ca91df19
Giuseppe Mendola <mendola5@hotmail.com>
2019-11-16 11:03:30 +0100

    Modify script to take the source data directory as argument

--- stationToJson.py
+++ stationToJson.py
@@ -4,26 +4,25 @@ import sys
 
 
 #input argument: direcory to the stations csv
-#output: structured dictionary
-def constructStationsDict(dir):
+#output: json representing with the needed json data
+def stationsToJSON():
     keysToSkip=['id','networkId','networkName']
     stationsDict={}
-    with open(dir+'stations.csv', newline='') as csvfile:
+    with open(sys.argv[0]+'stations.csv', newline='') as csvfile:
         stations = csv.DictReader(csvfile)
         for station in stations:
             stationId = station['id']
             keys=station.keys()
             for key in keys:
                 if key not in keysToSkip:
+                    print(key)
                     if stationId not in stationsDict:
                         innerDict={}
                         innerDict[key] = station[key]
                         stationsDict[stationId]=innerDict
                     else:
                         innerDict[key]=station[key]
-    return stationsDict
+    with open(dir+'stations.json', 'w') as f:
+        json.dump(stationsDict, f)        
 
-
-dir=sys.argv[1]
-with open(dir+'stations.json', 'w') as f:
-    json.dump(constructStationsDict(dir), f)
+stationsToJSON()
