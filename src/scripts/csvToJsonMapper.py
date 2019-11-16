3b3f09b7fcbbff8bd6ccb1c7095d5052aa507ce1
Giuseppe Mendola <mendola5@hotmail.com>
2019-11-16 16:11:19 +0100

    Refactor code

--- src/scripts/csvToJsonMapper.py
+++ src/scripts/csvToJsonMapper.py
@@ -4,7 +4,6 @@ import datetime
 import sys
 
 from os import listdir
-from os.path import isfile, join
 from stationToJson import constructStationsDict, mapStationNameToId
 
 dir = sys.argv[1]
@@ -38,13 +37,14 @@ bikeTypes = {
 
 ridesInformation = {}
 
-nameToId=mapStationNameToId(dir)
+nameToId = mapStationNameToId(dir)
 
-def collectData():
-    ridesDir = dir+'/rides/'
+
+def collect_data():
+    ridesDir = dir + '/rides/'
     for file in listdir(ridesDir):
         if file.endswith('.csv'):
-            with open(ridesDir+file, newline='') as csvfile:
+            with open(ridesDir + file, newline='') as csvfile:
                 rides = csv.DictReader(csvfile)
                 for ride in rides:
                     rideId = ride['Fahrt: Name']
@@ -58,50 +58,51 @@ def collectData():
                             innerDict[column] = ride[column]
                             ridesInformation[rideId] = innerDict
 
+
 # We remove entries which have less than 6 fields, as they can not have all the information we need
-
-
-def cleanData(ride):
+def clean_data(ride):
     if len(ridesInformation[ride].keys()) < 6:
         del ridesInformation[ride]
 
 
-def cleanKey(ride, key):
+def clean_key(ride, key):
     del ridesInformation[ride][key]
 
 
-def subTime(time, difference):
+def sub_time(time, difference):
     format = '%d.%m.%Y %H:%M'
     newTime = datetime.datetime.strptime(
         time, format) - datetime.timedelta(minutes=difference)
     return newTime.strftime(format)
 
-def getTimeRange(time):
-    hoursAndMins=time.split(':')
+
+def get_time_range(time):
+    hoursAndMins = time.split(':')
     hours = hoursAndMins[0]
     return hours
 
-def extractStartAndEndDate(ride):
+
+def extract_start_and_end_date(ride):
     endDate = ridesInformation[ride]['Ende']
     rideDurationString = ridesInformation[ride]['Ausleihdauer']
     rideDurationinMinutes = durationStringToMinutes[rideDurationString]
-    startDate = subTime(endDate, rideDurationinMinutes)
+    startDate = sub_time(endDate, rideDurationinMinutes)
     startDayAndTime = startDate.split(' ')
     startDay = startDayAndTime[0]
     startTime = startDayAndTime[1]
     ridesInformation[ride]['startDate'] = startDay
-    ridesInformation[ride]['startTime'] = getTimeRange(startTime)
+    ridesInformation[ride]['startTime'] = get_time_range(startTime)
     endDayAndTime = endDate.split(' ')
     endDay = endDayAndTime[0]
     endTime = endDayAndTime[1]
     ridesInformation[ride]['endDate'] = endDay
-    ridesInformation[ride]['endTime'] = getTimeRange(endTime)
+    ridesInformation[ride]['endTime'] = get_time_range(endTime)
     # clean up useless keys
     del ridesInformation[ride]['Ende']
     del ridesInformation[ride]['Ausleihdauer']
 
 
-def extractStartAndEndStation(ride):
+def extract_start_and_end_station(ride):
     startStation = ridesInformation[ride]['Station - VON']
     endStation = ridesInformation[ride]['Station - BIS']
     ridesInformation[ride]['startStation'] = startStation
@@ -110,7 +111,7 @@ def extractStartAndEndStation(ride):
     del ridesInformation[ride]['Station - BIS']
 
 
-def extractWeekDay(ride):
+def extract_week_day(ride):
     weekDay = ridesInformation[ride]['Ende Wochentag']
     ridesInformation[ride]['weekDay'] = weekDays[weekDay]
     del ridesInformation[ride]['Ende Wochentag']
@@ -126,71 +127,77 @@ def createHeatMap(dir):
     constructStationsDict(dir)
 
 
-def collectRidesByWeekDay(startOrEnd):
-    stationDict={}
+def collectRidesByStation(startOrEnd):
+    stationDict = {}
     for rideId in ridesInformation.keys():
-        ride=ridesInformation[rideId]
-        if ride[startOrEnd+'Station'] in nameToId:
-            stationId=nameToId[ride[startOrEnd+'Station']]
+        ride = ridesInformation[rideId]
+        if ride[startOrEnd + 'Station'] in nameToId:
+            stationId = nameToId[ride[startOrEnd + 'Station']]
             if stationId not in stationDict:
-                weekDayDict={}
+                weekDayDict = {}
             else:
-                weekDayDict=stationDict[stationId]
-                collectRidesByDay(startOrEnd,ride,weekDayDict)
-            stationDict[stationId]=weekDayDict
+                weekDayDict = stationDict[stationId]
+                collectRidesByWeekDay(startOrEnd, ride, weekDayDict)
+            stationDict[stationId] = weekDayDict
     return stationDict
 
 
-def collectRidesByDay(startOrEnd,ride,dayDict):
+def collectRidesByWeekDay(startOrEnd, ride, dayDict):
     if ride['weekDay'] not in dayDict:
-        hourDict={}
+        hourDict = {}
     else:
-        hourDict=dayDict[ride['weekDay']]
-    collectRideByHours(startOrEnd,ride,hourDict)
-    dayDict[ride['weekDay']]=hourDict
+        hourDict = dayDict[ride['weekDay']]
+    countRidesByHours(startOrEnd, ride, hourDict)
+    dayDict[ride['weekDay']] = hourDict
 
-def collectRideByHours(startOrEnd,ride,hourDict):
-    if ride[startOrEnd+'Time'] not in hourDict:
-        hourDict[ride[startOrEnd+'Time']]=1
+
+def countRidesByHours(startOrEnd, ride, hourDict):
+    if ride[startOrEnd + 'Time'] not in hourDict:
+        hourDict[ride[startOrEnd + 'Time']] = 1
     else:
-        hourDict[ride[startOrEnd+'Time']]+=1
+        hourDict[ride[startOrEnd + 'Time']] += 1
 
 
-def getHourDict(rideId,dateDict):
+def getHourDict(rideId, dateDict):
     ride = ridesInformation[rideId]
     startDate = ride['startDate']
     if startDate not in dateDict:
-        hourDict={}
+        hourDict = {}
     else:
-        hourDict=dateDict[startDate]
-    hourDict[ride['startTime']]=rideId
+        hourDict = dateDict[startDate]
+    hourDict[ride['startTime']] = rideId
     return hourDict
 
 
-
-collectData()
-for ride in ridesInformation.copy().keys():
-    cleanData(ride)
-    if ride in ridesInformation:
-        extractStartAndEndDate(ride)
-        extractStartAndEndStation(ride)
-        extractWeekDay(ride)
-        extractBikeType(ride)
-
-
 def getRidesDataToStationsValue():
-    startDict=collectRidesByWeekDay('start')
-    endDict=collectRidesByWeekDay('end')
-    stationsDict=constructStationsDict(dir)
+    startDict = divide_numbers_occurences(collectRidesByStation('start'))
+    endDict = divide_numbers_occurences(collectRidesByStation('end'))
+    stationsDict = constructStationsDict(dir)
     for stationId in stationsDict:
-        usageDict=collectRidesByWeekDay(stationId)
-        station=stationsDict[stationId]
-        station['usage']=startDict[stationId]
+        station = stationsDict[stationId]
+        station['loan'] = startDict[stationId]
+        station['returns'] = endDict[stationId]
+
     return stationsDict
 
-with open(dir+'/rides.json', 'w') as f:
-     json.dump(getRidesDataToStationsValue(), f)
 
+def divide_numbers_occurences(weekDayDictionary):
+    for weekDay in weekDayDictionary:
+        hoursDict = weekDayDictionary[weekDay]
+        for hour in hoursDict:
+            counters = hoursDict[hour]
+            for counter in counters:
+                counters[counter] = round(counters[counter] / 13,2)
+    return weekDayDictionary
 
-# with open('rides.json', 'w') as f:
-#     json.dump(ridesInformation, f)
+collect_data()
+for ride in ridesInformation.copy().keys():
+    clean_data(ride)
+    if ride in ridesInformation:
+        extract_start_and_end_date(ride)
+        extract_start_and_end_station(ride)
+        extract_week_day(ride)
+        extractBikeType(ride)
+
+with open(dir + '/rides.json', 'w') as f:
+    json.dump(getRidesDataToStationsValue(), f)
